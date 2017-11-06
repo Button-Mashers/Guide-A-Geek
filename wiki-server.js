7f56edf18d04c7bba33f9fbf97663916c2717feb
Hunter <edhunter83@gmail.com>
2017-11-05 19:48:46 -0500

    updates

--- wiki-server.js
+++ wiki-server.js
@@ -6,26 +6,34 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 app.get('/scrape', function(req, res) {
-	url = 'http://www.imdb.com/title/tt0082495/';
+	url = 'https://news.google.com/news/explore/section/q/Assassin%27s%20Creed%3A%20Origins/Assassin%27s%20Creed%3A%20Origins?ned=us&hl=en&gl=US';
 	request(url, function(error, response, html) {
 		if(!error) {
 			var $ = cheerio.load(html);
 
-			var summary;
-			var json = {summary: ""};
+			var text = [];
+			var articleLink = [];
+			var image = [];
+			var news = {text: "", articleLink: "", image: ""};
 
-			$('.summary_text').filter(function() {
-				var data = $(this);
-				summary = data.text();
-				json.summary = summary;
-			})
+			$('c-wiz.M1Uqc.kWyHVd').filter(function() {
+				text.push($(this).children().first().text());
+				articleLink.push($(this).children().first().attr("href"));
+				news.text = text;
+				news.articleLink = articleLink;
+			});
+
+			$('div.qx0yFc').each(function() {
+				image.push($(this).find("img").attr("src"));
+				news.image = image;
+			});
 		}
 
-		fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err) {
-			console.log('File successfully written! - Check your project directory for the output.json file');
-		});
+		// fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err) {
+		// 	console.log('File successfully written! - Check your project directory for the output.json file');
+		// });
 
-		res.send('Check your console!');
+		res.send(news);
 	})
 });
 
